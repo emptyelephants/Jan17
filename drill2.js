4a104102d063eaa0c5a76a14fb17b8fcbe263462
Christina Moore <sayhitochristina@gmail.com>
2018-01-17 11:21:11 -0600

    find by id

--- drill2.js
+++ drill2.js
@@ -17,31 +17,40 @@ function makeStudentReport(data) {
 
 function enrollInSummerSchool(students){
   for(let i = 0; i<students.length;i++){
-    students[i].status = 'In Summer School'
+    students[i].status = 'In Summer School';
   }
   return students;
 }
 
 
 let testData = [
-    {
-      name: 'Burt',
-      status: 'Playing hooky',
-      course: 'Biology',
-    },
-    {
-      name: 'Melanie',
-      status: 'Sick',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Leonard',
-      status: 'AWOL',
-      course: 'Computer science',
-    },
-  ];
+  {
+    name: 'Burt',
+    status: 'Playing hooky',
+    course: 'Biology',
+  },
+  {
+    name: 'Melanie',
+    status: 'Sick',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Leonard',
+    status: 'AWOL',
+    course: 'Computer science',
+  },
+];
 
 console.log(enrollInSummerSchool(testData));
 
+function findById(items, idNum) {
+  for ( let i = 0; i < items.length; i++) {
+    if (idNum === items[i].id) {
+      return items[i];
+    }
+  }
+}
 
+const data = [{id: 1, foo: 'bar'}, {id: 2, foo: 'bizz'}];
 
+console.log(findById(data,2));
