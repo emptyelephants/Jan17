10ddd7335b3c816f6b3a497ad254f148db70b1e6
Christina Moore <sayhitochristina@gmail.com>
2018-01-17 10:49:47 -0600

    person maker

--- drills1.js
+++ drills1.js
@@ -23,3 +23,18 @@ return obj;
 console.log(objectUpdater(firstObj));
 //console.log(createMyObject());
 
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+     return `${this.firstName} ${this.lastName}`;
+    }};
+  return person;
+}
+
+const myPerson = personMaker();
+console.log(personMaker().fullName());
