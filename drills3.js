72fb7886befc45d646383899b4960964226c47df
emptyelephants <emptyelephants@gmail.com>
2018-01-17 11:37:34 -0800

    Proerties that are not there

--- drills3.js
+++ drills3.js
@@ -6,7 +6,7 @@ const loaf = {
   }
 };
 
-console.log(loaf.hydration());
+//console.log(loaf.hydration());
 
 const objA = {
   foo:'foo-',
@@ -16,7 +16,7 @@ const objA = {
   spam:'spam-',
 }
 for(index in objA){
-  console.log(index+objA[index]);
+  //console.log(index+objA[index]);
 }
 // ask mentor how , for better understanding
 // for(let i=0;i<objA.length;i++){
@@ -27,27 +27,57 @@ for(index in objA){
 let food = {
   meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 }
-console.log(food.meals[3]);
+//console.log(food.meals[3]);
 
 let job1 = {
   name: 'John Doe',
-  jobTitle: 'survivor man'
+  jobTitle: 'survivor man',
+  boss:'Bumper'
 }
 
 let job2 = {
   name: 'Jane Doe',
-  jobTitle: 'survivor woman'
+  jobTitle: 'survivor woman',
+  boss:'Bumper'
 }
 
 let job3 = {
   name: 'Bumper',
   jobTitle: 'cat'
 }
+//part 4's
+// function jobsToArray(...jobs) {
+//   let jobArray = [job1, job2, job3];
+//   return jobArray;
+// }
+//(...items){ const [item1,item2,item3] = items; return items; }
+
+// console.log(jobsToArray(job1, job2, job3));
+
+
+//part 5
+//from warnings, tertiary string.
+//The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${warningCounter >1 ? 'times today!' : 'time today!'}`);
 
 function jobsToArray(...jobs) {
   let jobArray = [job1, job2, job3];
-  return jobArray;
+  let jobString =''
+  for(let i=0;i<jobArray.length;i++){
+   jobString+=`${jobArray[i].name} ${jobArray[i].jobTitle} ${jobArray[i].hasOwnProperty('boss') ? `reports to ${jobArray[i].boss}`:`reports to nobody`} \n`
+  }
+
+  return jobString.trim();
 }
-//(...items){ const [item1,item2,item3] = items; return items; }
+
 
 console.log(jobsToArray(job1, job2, job3));
+
+
+
+
+
+
+
+
+
+
